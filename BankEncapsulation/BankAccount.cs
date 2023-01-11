1b613d69e4c1ecb155cb7fa3dee78405c8e17f9a
Doug Finney <dugfiny@gmail.com>
2023-01-10 18:45:15 -0600

    ATM With Encapsulation

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+   public class BankAccount
+   {
+        //Constructor   
+        public BankAccount()
+        {
+
+        }
+
+        private double _balance = 0;
+
+        public void Deposit()
+        {
+            bool isTrue = false;
+            double amount;
+
+            do
+            {
+                Console.WriteLine("How much would you like to deposit?");
+                Console.Write("$");
+                isTrue = double.TryParse(Console.ReadLine(), out amount);
+
+            } while (!isTrue);
+
+            _balance += amount;
+
+             Console.WriteLine($"Deposit of ${amount} accepted\n");
+            
+        }
+        
+        public void GetBalance()
+        {
+            Console.WriteLine("Current Account Balance:");
+            Console.WriteLine($"${_balance}\n");
+
+
+        }
+   }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace BankEncapsulation
+﻿using System;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myAccount = new BankAccount();
+
+
+            myAccount.GetBalance();
+
+            myAccount.Deposit();
+
+            myAccount.GetBalance();
+        
         }
+            //Console.WriteLine("How much would you like to deposit?");
+            //Console.Write("$");
+            //var userInput = double.Parse(Console.ReadLine());
+
+            
+
+            //Console.WriteLine($"Current Balance: ${myAccount.GetBalance()}\n");
+            
+            //Console.WriteLine($"Current Balance: ${myAccount.GetBalance()}\n");
+         
+        
     }
+
+    
 }
